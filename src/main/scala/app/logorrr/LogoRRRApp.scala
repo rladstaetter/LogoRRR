e127c671c650bd7e9d5daa52c6ff7708f3aa83c7
Robert Ladstätter <rladstaetter@gmail.com>
2021-05-28 22:22:36 +0200

    #{8} : fix version number

--- src/main/scala/app/logorrr/LogoRRRApp.scala
+++ src/main/scala/app/logorrr/LogoRRRApp.scala
@@ -28,7 +28,7 @@ object LogoRRRApp {
   val ApplicationName = "LogoRRR"
 
   /** version which is displayed to user */
-  val ApplicationVersion = "21.2.3"
+  val ApplicationVersion = "21.2.4"
 
   /** initial width of main application scene */
   val InitialSceneWidth = 1000
