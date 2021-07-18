177eb14e4a65f9172f72a4cfd76d12fe25b1faeb
Robert Ladstätter <rladstaetter@gmail.com>
2021-07-18 14:14:46 +0200

    #27: use maven exec to call jlink/jpackage instead of script to stay in maven land

--- app/src/main/scala/app/logorrr/LogoRRRAppLauncher.scala
+++ app/src/main/scala/app/logorrr/LogoRRRAppLauncher.scala
@@ -2,7 +2,7 @@ package app.logorrr
 
 object LogoRRRAppLauncher {
 
-  /** launcher for macos installer */
+  /** launcher for macos installer to circumvent module loading mechanism by javafx (dirty hack) */
   def main(args: Array[String]): Unit = {
     LogoRRRApp.main(args)
   }
