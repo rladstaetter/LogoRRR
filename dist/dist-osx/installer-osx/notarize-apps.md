8497ed14ca0eab87864c0807503f111ea3f7104f
Robert Ladstätter <rladstaetter@gmail.com>
2021-10-13 19:53:14 +0200

    #36: gathering information about notarization process

---
+++ dist/dist-osx/installer-osx/notarize-apps.md
@@ -0,0 +1,4 @@
+# How to get the app into the appstore
+
+- Check notarization status, see https://stackoverflow.com/questions/55074168/how-do-i-verify-that-a-macos-pkg-is-notarized
+
---
+++ dist/dist-osx/installer-osx/verify-codesign.sh
@@ -0,0 +1,13 @@
+# https://medium.com/@andrew.perfiliev/how-to-verify-app-signatures-43fd5cd1bd3d
+
+# verify .app
+codesign -v -v target/temporary-app-image/LogoRRR.app
+
+# ths command should return:
+# target/temporary-app-image/LogoRRR.app: valid on disk
+# target/temporary-app-image/LogoRRR.app: satisfies its Designated Requirement
+
+# Another way to verify:
+# see https://stackoverflow.com/questions/26067694/code-has-no-resources-but-signature-indicates-they-must-be-present
+# spctl -at exec -vv target/temporary-app-image/LogoRRR.app/
+
