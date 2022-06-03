822f3406f42bfbf1064e8841f02a8281c7e3eecf
Robert Ladstätter <rladstaetter@gmail.com>
2022-06-03 21:54:36 +0200

    #85: fixes line number color

--- app/src/main/scala/app/logorrr/views/text/LineNumberLabel.scala
+++ app/src/main/scala/app/logorrr/views/text/LineNumberLabel.scala
@@ -1,11 +1,12 @@
 package app.logorrr.views.text
 
 import javafx.scene.control.Label
+import javafx.scene.paint.Color
 
 object LineNumberLabel {
 
 
-  def apply( lineNumber: Int, maxLength: Int): LineNumberLabel = {
+  def apply(lineNumber: Int, maxLength: Int): LineNumberLabel = {
     val ldl = new LineNumberLabel
     val string = lineNumber.toString.reverse.padTo(maxLength, " ").reverse.mkString
     ldl.setText(string)
@@ -13,4 +14,6 @@ object LineNumberLabel {
   }
 }
 
-class LineNumberLabel extends Label("")
+class LineNumberLabel extends Label("") {
+  setTextFill(Color.SLATEGREY)
+}
