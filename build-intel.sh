8265a3e8eeafbb5a6925e48ba0a732101e240e49
Robert Ladstätter <rladstaetter@gmail.com>
2022-08-07 14:29:31 +0200

    #85: adds comments to build-intel.sh script

--- build-intel.sh
+++ build-intel.sh
@@ -1,1 +1,5 @@
-mvn clean install; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-22.2.0-x86_64.pkg
+# arch has to be i386
+# this can be configured by calling
+# env /usr/bin/arch -x86_64 /bin/zsh --login
+# on aarch64 macs
+mvn clean install -T1C; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-22.2.0-x86_64.pkg
