b59419306c73c4ba9202fa7d727cde155d0530e3
Robert Ladstätter <rladstaetter@gmail.com>
2022-08-10 20:23:09 +0200

    #95: sets development version to 22.3.0-SNAPSHOT

--- build-intel.sh
+++ build-intel.sh
@@ -2,4 +2,4 @@
 # this can be configured by calling
 # env /usr/bin/arch -x86_64 /bin/zsh --login
 # on aarch64 macs
-mvn clean install -T1C; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-22.2.0-x86_64.pkg
+mvn clean install -T1C; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-22.3.0-x86_64.pkg
