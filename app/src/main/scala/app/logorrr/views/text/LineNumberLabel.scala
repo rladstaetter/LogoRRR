6620a5291c9b7d6fc80ea9a13a9f62a558f2dbc7
Robert Ladstätter <rladstaetter@gmail.com>
2022-10-15 14:37:22 +0200

    #101: improves visual difference of a log line entry and it's line number

--- app/src/main/scala/app/logorrr/views/text/LineNumberLabel.scala
+++ app/src/main/scala/app/logorrr/views/text/LineNumberLabel.scala
@@ -1,6 +1,8 @@
 package app.logorrr.views.text
 
+import javafx.geometry.Insets
 import javafx.scene.control.Label
+import javafx.scene.layout.{Background, BackgroundFill}
 import javafx.scene.paint.Color
 
 object LineNumberLabel {
@@ -16,4 +18,6 @@ object LineNumberLabel {
 
 class LineNumberLabel extends Label("") {
   setTextFill(Color.SLATEGREY)
+  setPadding(new Insets(0, 10, 0, 0))
+  setBackground(new Background(new BackgroundFill(Color.FLORALWHITE, null, null)))
 }
