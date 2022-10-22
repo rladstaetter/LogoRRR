0d7d651f81250a985bd3555a673adfafa58ccfc6
Robert Ladstätter <rladstaetter@gmail.com>
2022-10-22 14:07:17 +0200

    #108: sets version to 23.1.0-SNAPSHOT

--- build-intel.sh
+++ build-intel.sh
@@ -2,4 +2,4 @@
 # this can be configured by calling
 # env /usr/bin/arch -x86_64 /bin/zsh --login
 # on aarch64 macs
-mvn clean install -T1C; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-22.3.0-x86_64.pkg
+mvn clean install -POS.osx.x64 -T1C; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-23.1.0-x86_64.pkg
