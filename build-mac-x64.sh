5f653ae80653ae0d6bb96feca356b857fac9583d
Robert Ladstätter <rladstaetter@gmail.com>
2023-02-21 16:16:11 +0100

    #134: adds more documentation to build script for mac

--- build-mac-x64.sh
+++ build-mac-x64.sh
@@ -1,5 +1,12 @@
 # arch has to be i386
 # this can be configured by calling
+#
 # env /usr/bin/arch -x86_64 /bin/zsh --login
-# on aarch64 macs
+#
+# on aarch64 macs. After that, make sure you are using a x64 java to compile and build the application.
+#
+# You can easily manage your java installation with `sdkman` like this:
+#
+# sdk use java 19.ea.19-open
+#
 mvn clean install -POS.osx.x64 -T1C; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-23.2.0-x86_64.pkg
