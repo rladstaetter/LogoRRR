8302bb1816ee109cac9e013c36fc3a9bc5d78d8f
Robert Ladstätter <rladstaetter@gmail.com>
2023-12-23 10:32:04 +0100

    #187: bumps version to 24.2.0

--- build-linux-aarch64.sh
+++ build-linux-aarch64.sh
@@ -1,1 +1,1 @@
-mvn clean install -T1C; sudo apt install ./dist/dist-linux/installer-linux/target/installer/logorrr_24.1.0_arm64.deb; /opt/logorrr/bin/LogoRRR
+mvn clean install -T1C; sudo apt install ./dist/dist-linux/installer-linux/target/installer/logorrr_24.2.0_arm64.deb; /opt/logorrr/bin/LogoRRR
--- build-win.bat
+++ build-win.bat
@@ -2,4 +2,4 @@
 ECHO Building LogoRRR
 cmd /C mvn clean package -q -T1C
 ECHO Installing LogoRRR
-cmd /C .\dist\dist-win\installer-win-jre\target\installer\LogoRRR-24.1.0-installer-jre.exe
+cmd /C .\dist\dist-win\installer-win-jre\target\installer\LogoRRR-24.2.0-installer-jre.exe
