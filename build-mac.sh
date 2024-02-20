c569c688655cb96b03c174558de7ca87eb63deeb
Robert Ladstätter <rladstaetter@gmail.com>
2024-02-20 21:38:38 +0100

    #200: sets version to 24.3.0-SNAPSHOT

--- build-mac.sh
+++ build-mac.sh
@@ -1,1 +1,1 @@
-mvn clean install -T1C; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-24.2.0.pkg
+mvn clean install -T1C; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-24.3.0.pkg
