ed7645257b2f6cae80d41f6ce77c6685d37cafbb
Robert Ladstätter <rladstaetter@gmail.com>
2024-04-01 21:58:10 +0200

    #208: improves readability of TestFX based tests, adds test to open and close a file

--- app/src/main/scala/app/logorrr/views/LogoRRRNodes.scala
+++ app/src/main/scala/app/logorrr/views/LogoRRRNodes.scala
@@ -2,26 +2,26 @@ package app.logorrr.views
 
 case class LogoRRRNode(value: String) {
 
-  lazy val ref = "#" + value
+  lazy val ref: String = "#" + value
 
 }
 
 object LogoRRRNodes {
 
   /** id for the file menu */
-  val FileMenu = LogoRRRNode("file_menu")
+  val FileMenu: LogoRRRNode = LogoRRRNode("file_menu")
 
   /** ID of menu item which opens a file */
-  val FileMenuOpenFile = LogoRRRNode("file_menu_open_file")
+  val FileMenuOpenFile: LogoRRRNode = LogoRRRNode("file_menu_open_file")
 
   /**
    * close all files
    */
-  val FileMenuCloseAll = LogoRRRNode("file_menu_close_all")
+  val FileMenuCloseAll: LogoRRRNode = LogoRRRNode("file_menu_close_all")
 
   /**
    * ID of main tab pane where all log files are placed
    */
-  val MainTabPane = LogoRRRNode("main_tab_pane")
+  val MainTabPane: LogoRRRNode = LogoRRRNode("main_tab_pane")
 
 }
