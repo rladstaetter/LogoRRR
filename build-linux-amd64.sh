1bb46b5539d5e0bb409979d984d2997d640aae8e
Robert Ladstätter <rladstaetter@gmail.com>
2024-05-03 21:56:41 +0200

    #225: updates version to 24.4.0

--- build-linux-amd64.sh
+++ build-linux-amd64.sh
@@ -1,1 +1,1 @@
-mvn clean install -T1C; sudo apt install ./dist/dist-linux/installer-linux/target/installer/logorrr_24.3.0_amd64.deb; /opt/logorrr/bin/LogoRRR
+mvn clean install -T1C; sudo apt install ./dist/dist-linux/installer-linux/target/installer/logorrr_24.4.0_amd64.deb; /opt/logorrr/bin/LogoRRR
--- build-mac.sh
+++ build-mac.sh
@@ -1,1 +1,1 @@
-mvn clean install -T1C; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-24.3.0.pkg
+mvn clean install -T1C -Papp-tests; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-24.4.0.pkg
