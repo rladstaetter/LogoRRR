b19457f7bafe122fe05b219003bc200e22cec613
Robert Ladstätter <rladstaetter@gmail.com>
2024-05-15 22:28:57 +0200

    #29: activates digital signing for windows builds

--- build-win.bat
+++ build-win.bat
@@ -2,4 +2,4 @@
 ECHO Building LogoRRR
 cmd /C mvn clean package -q -T1C
 ECHO Installing LogoRRR
-cmd /C .\dist\dist-win\installer-win-jre\target\installer\LogoRRR-24.4.0-installer-jre.exe
+cmd /C .\dist\dist-win\installer-win-jre\target\installer\LogoRRR-24.4.0-installer.exe
