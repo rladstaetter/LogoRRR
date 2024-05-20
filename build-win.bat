51222baddbc1adc5cc0f0ab9e13f84a4bbbb9e3b
Robert Ladstätter <rladstaetter@gmail.com>
2024-05-20 22:34:36 +0200

    #29: install artifacts when building on windows

--- build-win.bat
+++ build-win.bat
@@ -1,5 +1,5 @@
 @echo off
 ECHO Building LogoRRR
-cmd /C mvn clean package -q -T1C
+cmd /C mvn clean install -q -T1C
 ECHO Installing LogoRRR
 cmd /C .\dist\dist-win\installer-win\target\installer\LogoRRR-24.4.0-installer.exe
