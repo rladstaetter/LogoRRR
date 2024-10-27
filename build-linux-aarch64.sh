199f063a2160d54437be3dfab831a343cfc8d7c4
Robert Ladstätter <rladstaetter@gmail.com>
2024-10-27 19:53:39 +0100

    #238: updates version to 25.1.0

--- build-linux-aarch64.sh
+++ build-linux-aarch64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mvn clean install -T1C
-sudo apt install ./dist/dist-linux/installer-linux/target/installer/logorrr_24.5.1_arm64.deb
+sudo apt install ./dist/dist-linux/installer-linux/target/installer/logorrr_25.1.0_arm64.deb
 /opt/logorrr/bin/LogoRRR
