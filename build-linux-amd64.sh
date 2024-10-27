946d077a33047cc1ec98d8f6ab8c7fb3257f0f1f
Robert Ladstätter <rladstaetter@gmail.com>
2024-10-27 14:41:25 +0100

    #238: adds shell declaration to build scripts

--- build-linux-amd64.sh
+++ build-linux-amd64.sh
@@ -1,1 +1,5 @@
-mvn clean install -T1C; sudo apt install ./dist/dist-linux/installer-linux/target/installer/logorrr_24.5.0_amd64.deb; /opt/logorrr/bin/LogoRRR
+#!/bin/bash
+
+mvn clean install -T1C
+sudo apt install ./dist/dist-linux/installer-linux/target/installer/logorrr_24.5.0_amd64.deb
+/opt/logorrr/bin/LogoRRR
--- build-mac.sh
+++ build-mac.sh
@@ -1,1 +1,4 @@
-mvn clean install -T1C -Papp-tests; open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-24.5.0.pkg
+#!/bin/bash
+
+mvn clean install -T1C -Papp-tests
+open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-24.5.0.pkg
