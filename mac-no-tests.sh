e0826a4488ecc4847d41ea2c4ff522adf5febc00
Robert Ladstätter <rladstaetter@gmail.com>
2025-08-06 20:54:09 +0200

    #276: fixes script parameter

--- mac-no-tests.sh
+++ mac-no-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-MAVEN_OPTS="--enable-native-access=ALL-UNNAMED" mvn clean install -T1C -Dmaven.test.skip=true
+MAVEN_OPTS="--enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow" mvn clean install -T1C -Dmaven.test.skip=true
 open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-25.1.0.pkg
