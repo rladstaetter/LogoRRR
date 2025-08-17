2eca5e1933c0eb23a4218f6654479cf0e00c771e
Robert Ladstätter <rladstaetter@gmail.com>
2025-08-17 23:17:12 +0200

    #305: moves size buttons to slider (also for TextView)

---
+++ app/src/main/scala/app/logorrr/views/ops/IncreaseSizeButton.scala
@@ -0,0 +1,35 @@
+package app.logorrr.views.ops
+
+import javafx.beans.property.Property
+import javafx.scene.Node
+
+object IncreaseSizeButton {
+
+  def apply(jfxId: String, graphic: Node, step: Int, max: Int, sizeProperty: Property[Number]): IncreaseSizeButton = {
+    val btn = new IncreaseSizeButton(jfxId, step, max, graphic)
+    btn.sizeProperty.bindBidirectional(sizeProperty)
+    btn
+  }
+
+}
+
+
+
+
+
+
+class IncreaseSizeButton(jfxId: String
+                         , step: Int
+                         , maxSize: Int
+                         , graphic: Node) extends SizeButton(graphic, "+") {
+  setId(jfxId)
+  setOnAction(_ => {
+    val nextSize = getSize.intValue + step
+    if (nextSize < maxSize) {
+      setSize(nextSize)
+    } else {
+      setSize(maxSize)
+    }
+  })
+
+}
--- app/src/main/scala/app/logorrr/views/text/toolbaractions/DecreaseTextSizeButton.scala
+++ app/src/main/scala/app/logorrr/views/text/toolbaractions/DecreaseTextSizeButton.scala
@@ -1,8 +1,6 @@
 package app.logorrr.views.text.toolbaractions
 
 import app.logorrr.io.FileId
-import app.logorrr.views.logfiletab.TextConstants
-import app.logorrr.views.ops.TextSizeButton
 import app.logorrr.views.{UiNode, UiNodeFileIdAware}
 
 object DecreaseTextSizeButton extends UiNodeFileIdAware {
@@ -11,12 +9,4 @@ object DecreaseTextSizeButton extends UiNodeFileIdAware {
 
 }
 
-class DecreaseTextSizeButton(val fileId: FileId) extends TextSizeButton(9, "decrease text size") {
-
-  setId(DecreaseTextSizeButton.uiNode(fileId).value)
-  setOnAction(_ => {
-    if (getFontSize - TextConstants.fontSizeStep > 0) {
-      setFontSize(getFontSize - TextConstants.fontSizeStep)
-    }
-  })
-}
+class DecreaseTextSizeButton
