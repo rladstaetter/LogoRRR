fd87108a67e02f2e3ae441244b16da19c9fe4d6b
Robert Ladstätter <rladstaetter@gmail.com>
2025-12-21 21:46:33 +0100

    #313: LogoRRR 25.2.0 macos app store submission

--- mac.sh
+++ mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-MAVEN_OPTS="--enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow" mvn clean install -T1C -Papp-tests
+mvn clean -T1C -Papp-tests
+MAVEN_OPTS="--enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow" mvn install -T1C -Papp-tests
 open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-25.2.0.pkg
