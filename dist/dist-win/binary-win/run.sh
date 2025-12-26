0be5d7b15b7432c0eb48f660a23ab6a2be598261
Robert Ladstätter <rladstaetter@gmail.com>
2025-12-27 00:28:40 +0100

    #334: removes graalvm build for windows

--- dist/dist-win/binary-win/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mvn clean package
-cp /Users/lad/gh/LogoRRR/native/native-osx/target/libLogoRRR.dylib target/gluonfx/aarch64-darwin/
-cp /Users/lad/gh/LogoRRR/native/native-osx/target/libLogoRRRSwift.dylib target/gluonfx/aarch64-darwin/
-cd target/gluonfx/aarch64-darwin/
-./app.logorrr.dist.win.binary
