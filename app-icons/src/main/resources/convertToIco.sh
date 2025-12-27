0b50db3579c3a37b199c2f5661c1b35dc52085d7
Robert Ladstätter <rladstaetter@gmail.com>
2025-12-27 23:03:46 +0100

    #332: redesigned icon

--- app-icons/src/main/resources/convertToIco.sh
+++
@@ -1,6 +0,0 @@
-convert logorrr-icon-128.png logorrr-icon-128.ico
-convert logorrr-icon-16.png logorrr-icon-16.ico
-convert logorrr-icon-256.png logorrr-icon-256.ico
-convert logorrr-icon-32.png logorrr-icon-32.ico
-convert logorrr-icon-512.png logorrr-icon-512.ico
-convert logorrr-icon-64.png logorrr-icon-64.ico
