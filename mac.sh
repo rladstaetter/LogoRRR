80d81334851a392fc7a59606d17d232bfdb148c4
Robert Ladstätter <rladstaetter@gmail.com>
2026-01-06 17:21:38 +0100

    #333: updates mac shell scripts to new approach

--- mac.sh
+++ mac.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mvn clean -T1C -Papp-tests
-mvn clean install -Papp-tests
+source "$(dirname "$0")/scripts/common.sh"
+build app.logorrr.dist.osx:installer-osx
+
 open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-26.1.0.pkg
