e54f987503ba794809f70641bd5d1de1e66647d1
Robert Ladstätter <rladstaetter@gmail.com>
2026-01-15 13:44:31 +0100

    #318: moves windows installer to new namespace

--- win.bat
+++ win.bat
@@ -1,6 +1,6 @@
 @echo off
 ECHO Building a clean LogoRRR Windows Installer
-cmd /C mvnw.cmd clean install -pl app.logorrr.dist:installer-win-jre -am -B
+cmd /C mvnw.cmd clean install -pl app.logorrr.dist:installer -am -B
 
 ECHO Execute LogoRRR installer (Run as Adminstrator ...)
 explorer .\dist\dist-win\installer-win-jre\target\installer\
