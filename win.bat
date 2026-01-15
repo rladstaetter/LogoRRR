0317ba732b65a66cb567cbe0d597b16316262f1a
Robert Ladstätter <rladstaetter@gmail.com>
2026-01-15 14:17:19 +0100

    #318: adds app-image zip distribution and reworks module structure for windows

--- win.bat
+++ win.bat
@@ -1,6 +1,6 @@
 @echo off
-ECHO Building a clean LogoRRR Windows Installer
-cmd /C mvnw.cmd clean install -pl app.logorrr.dist:installer -am -B
+ECHO Building a clean LogoRRR Windows Installer and zip distribution
+cmd /C mvnw.cmd clean install -pl app.logorrr.dist.win:installer,app.logorrr.dist.win:app-image -am -B
 
 ECHO Execute LogoRRR installer (Run as Adminstrator ...)
 explorer .\dist\dist-win\installer-win-jre\target\installer\
