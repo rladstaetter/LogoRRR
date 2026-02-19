ca56cf4709671555f326a3353c55a8711cf2314d
Robert Ladstätter <rladstaetter@gmail.com>
2026-02-19 20:57:11 +0100

    #333: adds images for release 26.1.0

--- docs/src/main/scala/app/logorrr/docs/ScreenShotterApp.scala
+++ docs/src/main/scala/app/logorrr/docs/ScreenShotterApp.scala
@@ -48,7 +48,7 @@ class ScreenShotterApp extends javafx.application.Application with TinyIo with T
 
         LogoRRRApp.start(stage, services)
 
-        val bPath = Paths.get(s"releases/${LogoRRRApp.appInfo.version}/")
+        val bPath = Paths.get(s"docs/releases/${LogoRRRApp.appInfo.version}/")
         createDirectories(bPath)
         val f = bPath.resolve(s"${w}x$h.png")
         ScreenShotterApp.persistNodeState(stage.getScene.getRoot, f)
