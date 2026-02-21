feecb02058e35e9bf93548524405d264794182fc
Robert Ladstätter <rladstaetter@gmail.com>
2026-02-21 16:46:24 +0100

    #364: adds uninstall.bat for 26.1.1

--- dist/win/installer/uninstall.bat
+++ dist/win/installer/uninstall.bat
@@ -1,4 +1,5 @@
-rem get-wmiobcet Win32_Product > installed_apps.txt
+rem get-wmiobject Win32_Product > installed_apps.txt
 
 msiexec /x {f27d1340-33ab-30a2-a9cd-f58ba17f3581} /L*V uninstall_log-25.1.0.txt
 msiexec /x {5b413bc5-35cb-3793-b588-2518728ca704} /L*V uninstall_log-26.1.0.txt
+msiexec /x {8a715c03-03a8-3b20-86f0-8697d4b9db26} /L*V uninstall_log-26.1.1.txt
