86b1bacdb55e2efb082cd1f55ce6ef03fbe1fcca
Robert Ladstätter <rladstaetter@gmail.com>
2026-03-03 18:36:48 +0100

    #366: renames dist/osx to dist/mac, fixes mac signing for javapackager25

--- mac.sh
+++ mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source "$(dirname "$0")/scripts/common.sh"
-build package app.logorrr.dist.osx:installer-osx
+build package app.logorrr.dist.mac:installer-mac
 
-open ./dist/dist-osx/installer-osx/target/installer/LogoRRR-26.2.0.pkg
+open ./dist/mac/installer-mac/target/installer/LogoRRR-26.2.0.pkg
