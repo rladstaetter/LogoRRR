80bb0ca63b7d57743a74c12a19955ca0b83a460e
Robert Ladstätter <rladstaetter@gmail.com>
2026-03-03 23:55:30 +0100

    #366: sets version to 26.2.1

--- mac.sh
+++ mac.sh
@@ -3,4 +3,4 @@
 source "$(dirname "$0")/scripts/common.sh"
 build package app.logorrr.dist.mac:installer-mac
 
-open ./dist/mac/installer-mac/target/installer/LogoRRR-26.2.0.pkg
+open ./dist/mac/installer-mac/target/installer/LogoRRR-26.2.1.pkg
