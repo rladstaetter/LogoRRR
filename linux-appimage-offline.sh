7a4ce44004fc18ac128079474d6eb524e0d2f773
Robert Ladstätter <rladstaetter@gmail.com>
2026-03-18 18:47:52 +0100

    #281: optimises offline script

--- linux-appimage-offline.sh
+++ linux-appimage-offline.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 echo "cleaning"
-./mvnw clean -pl "app.logorrr.dist.linux:app-image"
 rm -rf ./temp-repo
-
-echo "fetching openjfx"
-./mvnw generate-resources -pl "app.logorrr:env" -U
 
 echo "building"
 ./mvnw clean package -pl "app.logorrr.dist.linux:app-image" -am -B -Dmaven.repo.local=./temp-repo
