38365cd6d15b0a9260565bef73f8d4bdaab17e93
Robert Ladstätter <rladstaetter@gmail.com>
2026-03-18 20:14:09 +0100

    #281: uses maven for offline compilation to match flathubs setup

--- linux-appimage-offline.sh
+++ linux-appimage-offline.sh
@@ -3,7 +3,7 @@
 echo "cleaning"
 rm -rf ./temp-repo
 
-./mvnw process-resources -pl "app.logorrr:env"
+mvn process-resources -pl "app.logorrr:env"
 
 echo "building"
-./mvnw clean package -pl "app.logorrr.dist.linux:app-image" -am -B -Dmaven.repo.local=./temp-repo
+mvn clean package -pl "app.logorrr.dist.linux:app-image" -am -B -Dmaven.repo.local=./temp-repo -Denforcer.skip=true
