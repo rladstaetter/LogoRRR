499f3a386a6e4377dbff416e6cf77517f9d3fa22
Robert Ladstätter <rladstaetter@gmail.com>
2026-03-18 19:45:03 +0100

    #281: removes unecessary dependencies to build module

--- linux-appimage-offline.sh
+++ linux-appimage-offline.sh
@@ -3,5 +3,7 @@
 echo "cleaning"
 rm -rf ./temp-repo
 
+./mvnw process-resources -pl "app.logorrr:env"
+
 echo "building"
 ./mvnw clean package -pl "app.logorrr.dist.linux:app-image" -am -B -Dmaven.repo.local=./temp-repo
